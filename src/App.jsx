ed6ba44dedbd064802949a71220729599a8465f1
nitishkumar-13 <nitishkumars1302.com>
2024-07-28 18:18:12 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,57 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Navigate, Routes, redirect } from 'react-router-dom'
-import { Layout } from 'antd'
-import { AuthProvider } from "./context/AuthContext"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import AddRecipe from "./pages/AddRecipe"
-import UpdateRecipe from "./pages/UpdateRecipe"
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+  redirect,
+} from "react-router-dom";
+import { Layout } from "antd";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AddRecipe from "./pages/AddRecipe";
+import UpdateRecipe from "./pages/UpdateRecipe";
 // import './App.css'
 
 const { Content } = Layout;
 
 function App() {
-
-  const isAuthenticated = !!localStorage.getItem('token');
+  const isAuthenticated = !!localStorage.getItem("token");
+  console.log(isAuthenticated, "ISAuthenticated");
 
   return (
     <AuthProvider>
       <Router>
         <Layout>
           <Navbar />
-          <Content style={{ padding: '0 50px', marginTop: '64px' }}>
+          <Content style={{ padding: "0 50px", marginTop: "64px" }}>
             <Routes>
-              <Route path='/login' element={<Login />}></Route>
-              <Route path='/register' element={<Register />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/register" element={<Register />}></Route>
               {isAuthenticated ? (
                 <>
-                  <Route path='/' element={<Home />}></Route>
-                  <Route path='/add-recipe' element={<AddRecipe />}></Route>
-                  <Route path="/update-recipe/:recipeId" element={<UpdateRecipe />}></Route>
+                  <Route path="/" element={<Home />}></Route>
+                  <Route path="/add-recipe" element={<AddRecipe />}></Route>
+                  <Route
+                    path="/update-recipe/:recipeId"
+                    element={<UpdateRecipe />}
+                  ></Route>
                   {/* <Route path="*" element={<Navigate to="/" />} /> */}
                 </>
-              ) : (<Route path="*" element={<Navigate to="/login" />} />)}
+              ) : (
+                <Route path="*" element={<Navigate to="/login" />} />
+              )}
 
               {/* <Route path='/' element={<Navigate to="/login"></Navigate>}></Route> */}
             </Routes>
           </Content>
         </Layout>
-      </Router >
+      </Router>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,81 +1,93 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { Dropdown, Layout, Menu, message, Button } from 'antd'
-import axios from 'axios'
-import { AuthContext } from '../context/AuthContext'
-import { apiUrl } from '../utils/api'
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Dropdown, Layout, Menu, message, Button } from "antd";
+import axios from "axios";
+import { AuthContext } from "../context/AuthContext";
+import { apiUrl } from "../utils/api";
 
 const { Header } = Layout;
 
 function Navbar() {
-    const { user, logout } = useContext(AuthContext);
-    // const [user, setUser] = useState(null);
-    const navigate = useNavigate();
+  const { user, logout } = useContext(AuthContext);
+  // const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            try {
-                const response = await axios.get(`${apiUrl}/apiAuth/user/details`, {
-                    headers: {
-                        'Authorization': `Bearer ${localStorage.getItem('token')}`
-                    }
-                });
-                setUser(response.data);
-            } catch (error) {
-                console.error('Error fetching user details:', error);
-            }
-        };
-        fetchUserDetails();
-    }, []);
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/apiAuth/user/details`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+    fetchUserDetails();
+  }, []);
 
-    const handleLogout = () => {
-        // localStorage.removeItem('token');
-        // setUser(null);
-        logout();
-        message.success('Successfully logged out!');
-        navigate('/login');
-    }
+  const handleLogout = () => {
+    // localStorage.removeItem('token');
+    // setUser(null);
+    logout();
+    message.success("Successfully logged out!");
+    navigate("/login");
+  };
 
-    const menu = (
-        <Menu>
-            <Menu.Item key='logout' onClick={handleLogout}>
-                Logout
+  const menu = (
+    <Menu>
+      <Menu.Item key="logout" onClick={handleLogout}>
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
+  return (
+    <Header>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+        {user && user.role === "admin" && (
+          <>
+            <Menu.Item key="1">
+              <Link to="/">Home</Link>
             </Menu.Item>
-        </Menu>
-    )
-    return (
-        <Header>
-            <div className='logo' />
-            <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
-                {user && user.role === 'admin' && (
-                    <>
-                        <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-                        <Menu.Item key="4"><Link to="/add-recipe">Add Recipe</Link></Menu.Item>
-                        <Menu.Item key="5"><Link to="/update-recipe">Update Recipe</Link></Menu.Item>
-                    </>
-                )}
-                {user && user.role === 'user' && (
-                    <>
-                        <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-                    </>
-                )}
-                {!user && (
-                    <>
-                        <Menu.Item key="2"><Link to="/login">Login</Link></Menu.Item>
-                        <Menu.Item key="3"><Link to="/register">Register</Link></Menu.Item>
-                    </>
-                )}
-                {user && (
-                    <Menu.Item key="user" style={{ marginLeft: 'auto' }}>
-                        {user.username} ({user.role})
-                        <Dropdown overlay={menu} placement='bottomRight'>
-                            <Button type="link">Logout</Button>
-                        </Dropdown>
-                    </Menu.Item>
-                )}
-            </Menu>
-        </Header>
-    )
+            <Menu.Item key="4">
+              <Link to="/add-recipe">Add Recipe</Link>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <Link to="/update-recipe">Update Recipe</Link>
+            </Menu.Item>
+          </>
+        )}
+        {user && user.role === "user" && (
+          <>
+            <Menu.Item key="1">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+          </>
+        )}
+        {!user && (
+          <>
+            <Menu.Item key="2">
+              <Link to="/login">Login</Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Link to="/register">Register</Link>
+            </Menu.Item>
+          </>
+        )}
+        {user && (
+          <Menu.Item key="user" style={{ marginLeft: "auto" }}>
+            {user.username} ({user.role})
+            <Dropdown overlay={menu} placement="bottomRight">
+              <Button type="link">Logout</Button>
+            </Dropdown>
+          </Menu.Item>
+        )}
+      </Menu>
+    </Header>
+  );
 }
 
-export default Navbar
+export default Navbar;
